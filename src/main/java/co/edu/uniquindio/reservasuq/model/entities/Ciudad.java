ee3845abc90fdcd118cbc9dcb8248790e7534068
Juanda2312 <juand.tapierom@uqvirtual.edu.co>
2025-05-22 17:40:39 -0500

    Datos quemados

--- src/main/java/co/edu/uniquindio/reservasuq/model/entities/Ciudad.java
+++ src/main/java/co/edu/uniquindio/reservasuq/model/entities/Ciudad.java
@@ -3,5 +3,5 @@ package co.edu.uniquindio.reservasuq.model.entities;
 import java.io.Serializable;
 
 public enum Ciudad implements Serializable {
-    ARMENIA,PEREIRA,MEDELLIN,BOGOTA,CALI,PALMIRA;
+    ARMENIA,PEREIRA,MEDELLIN,BOGOTA,CALI,PALMIRA,SANANDRES;
 }
